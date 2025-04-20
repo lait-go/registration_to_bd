fd047eb708b520f1dbe534a84266064bf31d4af1
Lyanna Winston <lyannawi@de-c1.mag.21-school.ru>
2025-04-20 16:26:27 +0300

    add_log

--- cmd/main.go
+++ cmd/main.go
@@ -4,31 +4,33 @@ import (
 	conf "back/config"
 	"back/db"
 	handler "back/internal/api"
+	Error "back/internal/err"
 	LogWork "back/internal/utils/log"
 	"fmt"
-	"log"
-
 	"net/http"
 )
 
 func main() {
 	conf.Cfg = conf.Config_work()
-	logger := LogWork.LogInit()
-	
-	logger.Info("конфигурация считана")
-	logger.Info("логер запущен")
-	
-	db.DbExistCheck()
-	defer db.DB.Close()
-	
-	logger.Info("база данных проверена")
 
+	logger := LogWork.LogInit()
+	logger.Info("Логгер инициализирован")
 	
+	logger.Info("Проверка существования базы данных")
+	db.DbExistCheck()
+	defer func() {
+		logger.Info("Отключение от базы данных")
+		db.DB.Close()
+	}()
+
+	logger.Info("База данных успешно проверена и подключена")
+
 	http.HandleFunc("/", handler.Handler)
-	
-	logger.Info(fmt.Sprintf("Сервер запущен на %s", conf.Cfg.Host))
+	logger.Info("Маршруты HTTP зарегистрированы")
+
+	logger.Info(fmt.Sprintf("Сервер запускается на %s", conf.Cfg.Host))
 	err := http.ListenAndServe(conf.Cfg.Host, nil)
-	if err != nil {
-		log.Fatal(err)
+	if Error.GetErr(err) {
+		logger.Fatal(fmt.Sprint(err))
 	}
 }
